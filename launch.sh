260a1a9eeafff3447e29359bf30d2e1f13e9baff
Raja SR <nichecloudexpert@gmail.com>
2019-05-20 16:54:47 +0530

    Update Code

--- launch.sh
+++ launch.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
-cd ./openjdk11
-sleep 5
-docker build -t openjdk11-alpine:v1 .
-cd ..
-
-cd ./mysql5.7
-docker build -t  mysql5.7:v1 .
-cd ..
-sleep 5
-
+docker build -t openjdk11-alpine:v1 ./openjdk11/.
+sleep 10
+docker build -t  mysql5.7:v1 ./mysql5.7..
+sleep 10
 docker build -t tomcat-jdk11:v1 .
-sleep 5
-
-docker-compose up
+sleep 10
+docker-compose up -d
